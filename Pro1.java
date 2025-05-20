045ef4968ce19772394d15a2f44812c00fc0efa4
LOKI <lokeshll6848948@gmail.com>
2025-05-20 16:21:32 +0530

    updated version of Pro1

--- Pro1.java
+++ Pro1.java
@@ -3,7 +3,12 @@ public class Pro1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi Panda");
+		System.out.println("Hi Panda Lokesh");
+		for(int i=0;i<10;i++) {
+			System.out.println("Panda");
+		}
+		
+		System.out.println("Thanks for tracking the files using the git bash");
 	}
 
 }
